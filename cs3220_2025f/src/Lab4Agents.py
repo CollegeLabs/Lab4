5e985436698ccb52bbeb445fc35ba1fc8ff525be
GABRIEL NELLIS <5089401@nwpolytech.ca>
2025-10-16 01:53:22 -0600

    why does this not want to work :(

--- cs3220_2025f/src/Lab4Agents.py
+++ cs3220_2025f/src/Lab4Agents.py
@@ -1,5 +1,6 @@
 from src.agents import ProblemSolvingMazeAgentBFS
-
+import numpy as np
+import random
 
 
 class UniformCostAgent():
@@ -9,3 +10,8 @@ class UniformCostAgent():
 class IterativeDLSAgent():
     def __init__(self):
         self.performance = 0
+
+class EnemyShip():
+    def __init__(self):
+        size=7
+        self.power = (random.randint(10, 40)*(size*size))/100
--- cs3220_2025f/src/Lab4Environment.py
+++ cs3220_2025f/src/Lab4Environment.py
@@ -5,12 +5,13 @@ from src.PS_agentPrograms import BestFirstSearchAgentProgram
 from src.mazeProblemClass import MazeProblem
 from src.agents import ProblemSolvingMazeAgentBFS
 from src.naigationEnvironmentClass import MazeNavigationEnvironment
+from src.Lab4Agents import EnemyShip
 
 def makeMaze(n):
   size = (n,n)
   proba_0 =0.25 # resulting array will have 25%? of zeros
   proba_Enemy =0.1 # resulting array will have 10%? of enemy
-  arrMaze=np.random.choice([0, 1, 2], size=size, p=[proba_0, 1-proba_0-proba_Enemy, proba_Enemy] )
+  arrMaze=np.random.choice([0, 1, 2], size=size, p=[proba_0, 1-proba_0-proba_Enemy, proba_Enemy] ) #0=asteroid, 1=open, 2=enemy
   return arrMaze
 
 def MazeCheck(mainMaze, initState, goalState):
@@ -19,7 +20,34 @@ def MazeCheck(mainMaze, initState, goalState):
   if (goalState != 1):
     mainMaze[goalState] = 1
 
+class Lab4NavEnvironment(MazeNavigationEnvironment):
+  def __init__(self, navGraph, maze):
+    super().__init__(navGraph)
+    self.maze = maze
 
+  def execute_action(self, agent, action): #action and state are backwards here
+      '''Check if agent alive, if so, execute action'''
+      if self.is_agent_alive(agent):
+          """Change agent's location -> agent's state;
+          Track performance.
+          -1 for each move."""
+          
+          agent.state=agent.update_state(agent.state, action)
+          print(f"Agent in {agent.state} with performance = {agent.performance}")
+          print(f"Action = {action} and State = {agent.state}")
+          (x, y) = action
+          if (self.maze[x][y] == 1): #if the agent is NOT under attack
+            agent.performance -= 1
+          else: #the spaceship is under attack (0's are walls, so they're ignored before this)
+            print(f"Agent in {agent.state} is under attack!")
+            enemy = EnemyShip() #dont know how else to put size in without requiring it for ~3 other functions
+            if (agent.performance < enemy.power):
+              agent.performance = 0 #dies instantly
+            else:
+              agent.performance = agent.performance*0.9 #removes 10% of performance
+          self.update_agent_alive(agent)
+
+'''
 mazeSize=7
 mainMaze = makeMaze(mazeSize)
 mazeAvalActs=defineMazeAvailableActions(mainMaze)
@@ -30,3 +58,4 @@ initState = (0,0)
 goalState = (6,6)
 
 mp1=MazeProblem(initState, goalState, mazeWorldGraph)
+'''
