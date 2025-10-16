db9fd64e7cfb8c9a00892b07983fc36fd2c2f622
ConnorQR <5050771@nwpolytech.ca>
2025-10-15 20:47:50 -0600

    added function to ensure start and end are always available

--- cs3220_2025f/src/Lab4Environment.py
+++ cs3220_2025f/src/Lab4Environment.py
@@ -1,2 +1,32 @@
+from src.mazeData import *
+from src.maze2025GraphClass import mazeGraph
+from pyvis.network import Network
+from src.PS_agentPrograms import BestFirstSearchAgentProgram
+from src.mazeProblemClass import MazeProblem
+from src.agents import ProblemSolvingMazeAgentBFS
 from src.naigationEnvironmentClass import MazeNavigationEnvironment
 
+def makeMaze(n):
+  size = (n,n)
+  proba_0 =0.25 # resulting array will have 25%? of zeros
+  proba_Enemy =0.1 # resulting array will have 10%? of enemy
+  arrMaze=np.random.choice([0, 1, 2], size=size, p=[proba_0, 1-proba_0-proba_Enemy, proba_Enemy] )
+  return arrMaze
+
+def MazeCheck(mainMaze, initState, goalState):
+  if (initState != 1):
+    mainMaze[initState] = 1
+  if (goalState != 1):
+    mainMaze[goalState] = 1
+
+
+mazeSize=7
+mainMaze = makeMaze(mazeSize)
+mazeAvalActs=defineMazeAvailableActions(mainMaze)
+maze1TM=makeMazeTransformationModel(mazeAvalActs)
+mazeWorldGraph=mazeGraph(maze1TM, mazeStatesLocations(list(maze1TM.keys())))
+
+initState = (0,0)
+goalState = (6,6)
+
+mp1=MazeProblem(initState, goalState, mazeWorldGraph)
