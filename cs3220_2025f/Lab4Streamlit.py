ef1c32549dde2ae2ad051951b2e0bd6150546939
GABRIEL NELLIS <5089401@nwpolytech.ca>
2025-10-16 20:11:22 -0600

    testing streamlit thing

--- cs3220_2025f/Lab4Streamlit.py
+++ cs3220_2025f/Lab4Streamlit.py
@@ -56,12 +56,14 @@ def buildGraph(graphData, nodeColorsDict):
         width = "100%" 
     )
     net_maze.toggle_physics(False)
+    '''
     nodes=graphData.nodes()
     # initialize graph
     g = nx.Graph()
     
     # add the nodes
     for node in nodes:
+        print(node)
         g.add_node(node, color=nodeColorsDict[node])
     edges=[]
     for node_source in graphData.nodes():
@@ -69,12 +71,45 @@ def buildGraph(graphData, nodeColorsDict):
             if set((node_source,node_target)) not in edges:
                 edges.append(set((node_source,node_target)))                
     g.add_edges_from(edges)
+    '''
     
     # generate the graph
-    net_maze.from_nx(g)
+    #net_maze.from_nx(g)
+    nodes=["-".join(str(item) for item in el) for el in mazeWorldGraph.origin.keys()]
+
+    x_coords = []
+    y_coords = []
+
+    for node in mazeWorldGraph.origin.keys():
+        x,y=mazeWorldGraph.getLocation(node)
+        x_coords.append(x)
+        y_coords.append(y)
+
+    sizes=[10]*len(nodes)
+    net_maze.add_nodes(nodes, color=nodeColorsList, x=x_coords, y=y_coords, size=sizes, title=nodes)
+
+    for node in net_maze.nodes:
+        node['label']=''
+
+    edge_weights = {(intTupleTostr(k), intTupleTostr(v2)) : k2 for k, v in mazeWorldGraph.origin.items() for k2, v2 in v.items()}
+
+    edges=[]
+
+    for node_source in mazeWorldGraph.nodes():
+        for node_target, action in mazeWorldGraph.get(node_source).items():
+            #node_target or node_source is a tuple -> convert to str
+            if (intTupleTostr(node_source),intTupleTostr(node_target)) not in edges and (intTupleTostr(node_target), intTupleTostr(node_source)):
+                net_maze.add_edge(intTupleTostr(node_source),intTupleTostr(node_target), label=edge_weights[(intTupleTostr(node_source),intTupleTostr(node_target))])
+                edges.append((intTupleTostr(node_source),intTupleTostr(node_target)))
+
+    for node in net_maze.nodes:
+        if node['id']==intTupleTostr(goalState):
+            node['color']=nodeColors['Goal']
+        elif node['id']==intTupleTostr(initState):
+            node['color']=nodeColors['Start']
     
-    net_maze.save_graph('L3_RomaniaMap.html')
-    HtmlFile = open(f'L3_RomaniaMap.html', 'r', encoding='utf-8')
+    net_maze.save_graph('Maze Navigation.html')
+    HtmlFile = open(f'Maze Navigation.html', 'r', encoding='utf-8')
     components.html(HtmlFile.read(), height = 1200,width=1000)
     
     
